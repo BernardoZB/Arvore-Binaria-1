a875a547f5c7c2170bbf882b2687c494f3d9257b
BernardoZB <60757419+BernardoZB@users.noreply.github.com>
2022-08-05 02:07:10 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,26 @@
+
+using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        // Cria uma nova arvore começando pela raizuma
+        TreeNode root = new TreeNode(13);
+        // Adiciona valores na arvore
+        root.Add(5);
+        root.Add(15);
+        root.Add(33);
+        root.Add(1);
+        root.Add(3);
+        root.Add(2);
+        root.Add(0);
+        // Desenha a arvore completa
+        root.Print();
+        // Procura se existe um valor
+        Console.WriteLine("\n" + root.Find(2));
+        // Retorna a altura da arvore
+        Console.WriteLine(root.Altura());
+
+    }
+}
