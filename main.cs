e6d65bfba613612e7cabbfb33db2f5cf42189062
BernardoZB <60757419+BernardoZB@users.noreply.github.com>
2022-08-05 11:36:36 +0000

    Criação do menu

--- main.cs
+++ main.cs
@@ -1,19 +1,63 @@
 using System;
 
 class Program {
-  public static void Main (string[] args) {
+  public static void Main(string[] args) {
 
-    Console.Write("\nAdicione um nó: ");
+    Console.Write("\nAdicione uma raiz: ");
     // Cria uma nova arvore começando pela raizuma
-    TreeNode root = new TreeNode(int.Parse(Console.ReadLine()));
+    string input = Console.ReadLine();
+    TreeNode root = new TreeNode(int.Parse(input));
     
-    for(;;) {
-      Console.Clear();
-      // Desenha a arvore completa
+    
+    bool continuar = true;
+
+    while (continuar) {
       root.Print();
-      Console.Write("\n\nAdicione um nó: ");
-      // Adiciona valores na arvore
-      root.Add(int.Parse(Console.ReadLine()));
+
+      Console.WriteLine("\n\n**************************");
+      Console.WriteLine("| Escolha uma ação:      |");
+      Console.WriteLine("|   1. Adicionar         |");
+      Console.WriteLine("|   2. Remover           |");
+      Console.WriteLine("|   3. Achar             |");
+      Console.WriteLine("|   4. Sair              |");
+      Console.WriteLine("**************************\n");
+
+      input = Console.ReadLine();
+      int option = int.Parse(input);
+      switch (option) {
+        case 1:
+          for (;;) {
+            Console.Clear();
+            // Desenha a arvore completa
+            root.Print();
+            Console.Write("\n\nAdicione um nó (digite 'voltar' para retornar): ");
+            // Adiciona valores na arvore
+            input = Console.ReadLine();
+            if (input == "voltar") {
+              break;
+            }
+            root.Add(int.Parse(input));
+          }
+          Console.Clear();
+          break;
+        case 2:
+          Console.Clear();
+          root.Print();
+          Console.Write("\n\nRemover: ");
+          input = Console.ReadLine();
+          root.Remove(int.Parse(input));
+          break;
+        case 3:
+          Console.Clear();
+          root.Print();
+          Console.Write("\nAchar: ");
+          input = Console.ReadLine();
+          root.Find(int.Parse(input));
+          break;
+        case 4:
+          continuar = false;
+          break;
+      }
     }
   }
 }
